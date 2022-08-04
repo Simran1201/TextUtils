6a1c3ebf81ae700ca55f5610c04506a61a1cbe9e
Simran Prasad <71308532+Simran1201@users.noreply.github.com>
2022-08-04 12:20:50 +0530

    Updates

---
+++ precache-manifest.e1cd2c9bd8a0d1186b9e32425050397f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c19a8906e52c8365cf60",
+    "url": "/textUtils/static/css/main.e1551932.chunk.css"
+  },
+  {
+    "revision": "c19a8906e52c8365cf60",
+    "url": "/textUtils/static/js/main.c19a8906.chunk.js"
+  },
+  {
+    "revision": "97af9aaba510c3e4c76e",
+    "url": "/textUtils/static/js/1.97af9aab.chunk.js"
+  },
+  {
+    "revision": "0990596b2221aee43234",
+    "url": "/textUtils/static/js/2.0990596b.chunk.js"
+  },
+  {
+    "revision": "3bceb5ca18eee152a960",
+    "url": "/textUtils/static/js/runtime~main.3bceb5ca.js"
+  },
+  {
+    "revision": "b49e65e6aaad86c1b6157a6322d48f57",
+    "url": "/textUtils/index.html"
+  }
+];
